5e74d1449d42ebaa337161774611ca0b01af4956
ViniciusGabriel9898 <101222897+ViniciusGabriel9898@users.noreply.github.com>
2023-01-10 19:51:14 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,17 @@
+import firebase from "firebase";
+require("@firebase/firestore");
+
+var firebaseConfig = {
+  const firebaseConfig = {
+    apiKey: "AIzaSyAhWcN2-NJM0dTCZcOMOknu1p0_0nLbJLQ",
+    authDomain: "atividade-75.firebaseapp.com",
+    projectId: "atividade-75",
+    storageBucket: "atividade-75.appspot.com",
+    messagingSenderId: "136101265513",
+    appId: "1:136101265513:web:e2872f396a3ddefe5d62f8"
+  };
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebase.firestore();
